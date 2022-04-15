659b66cbcf625179302de1017341d83cec5c92e1
yuri-italo <yuri.italo94@gmail.com>
2022-04-14 17:59:22 -0700

    Category entity created.

---
+++ src/main/java/br/com/alura/aluraflixapi/model/Category.java
@@ -0,0 +1,40 @@
+package br.com.alura.aluraflixapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String color;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
