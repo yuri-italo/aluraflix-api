05cb69e3dd4da6bf9c7f74119f67dee9927a58a3
yuri-italo <yuri.italo94@gmail.com>
2022-06-19 11:40:34 -0300

    UserController is now responsible for returning Responses Entities.

--- src/main/java/br/com/alura/aluraflixapi/controller/UserController.java
+++ src/main/java/br/com/alura/aluraflixapi/controller/UserController.java
@@ -1,19 +1,24 @@
 package br.com.alura.aluraflixapi.controller;
 
+import br.com.alura.aluraflixapi.dto.UserDto;
 import br.com.alura.aluraflixapi.form.LoginForm;
+import br.com.alura.aluraflixapi.model.User;
 import br.com.alura.aluraflixapi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -31,21 +36,38 @@ public class UserController {
     @Transactional
     @Operation(summary = "Create a new user.")
     public ResponseEntity<?> save(@RequestBody @Valid LoginForm loginForm, UriComponentsBuilder uriBuilder) {
-        return userService.save(loginForm,uriBuilder);
+        Optional<User> optional = userService.findByEmail(loginForm.getEmail());
+
+        if (optional.isPresent())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists.");
+
+        User user = userService.save(loginForm);
+
+        return ResponseEntity.created(uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri()).body(new UserDto(user));
     }
 
     @GetMapping
     @Operation(summary = "Return a list of all users.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> getAll(@PageableDefault(sort = "id",size = 5,direction = Sort.Direction.ASC) Pageable pageable) {
-        return userService.listAll(pageable);
+        Page<User> list = userService.listAll(pageable);
+
+        if (list.hasContent())
+            return ResponseEntity.status(HttpStatus.OK).body(UserDto.convertManyToDto(list));
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get an user by ID.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> getOneById(@PathVariable Long id) {
-        return userService.getById(id);
+        Optional<User> optional = userService.findById(id);
+
+        if (optional.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(new UserDto(optional.get()));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found.");
     }
 
     @PutMapping("/{id}")
@@ -53,14 +75,34 @@ public class UserController {
     @Operation(summary = "Update one or more user fields.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<?> update(@RequestBody @Valid LoginForm loginForm, @PathVariable Long id) {
-        return userService.updateById(loginForm,id);
+        Optional<User> optional = userService.findById(id);
+
+        if (optional.isPresent()) {
+            Optional<User> userByEmail = userService.findByEmail(loginForm.getEmail());
+
+            if (userByEmail.isPresent())
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exist.");
+
+            User updatedUser = userService.update(loginForm, id);
+
+            return ResponseEntity.status(HttpStatus.OK).body(new UserDto(updatedUser));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found!");
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     @Operation(summary = "Delete an user by ID.")
     @SecurityRequirement(name = "bearerAuth")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        return userService.deleteById(id);
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        Optional<User> optional = userService.findById(id);
+
+        if (optional.isPresent()) {
+            userService.delete(optional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("User deleted.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found.");
     }
 }
--- src/main/java/br/com/alura/aluraflixapi/service/UserService.java
+++ src/main/java/br/com/alura/aluraflixapi/service/UserService.java
@@ -1,17 +1,14 @@
 package br.com.alura.aluraflixapi.service;
 
-import br.com.alura.aluraflixapi.dto.UserDto;
 import br.com.alura.aluraflixapi.form.LoginForm;
 import br.com.alura.aluraflixapi.model.User;
 import br.com.alura.aluraflixapi.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
@@ -24,12 +21,7 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public ResponseEntity<?> save(LoginForm loginForm, UriComponentsBuilder uriBuilder) {
-        Optional<User> optional = userRepository.findByEmail(loginForm.getEmail());
-
-        if (optional.isPresent())
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists.");
-
+    public User save(LoginForm loginForm) {
         loginForm.setPassword(new BCryptPasswordEncoder().encode(loginForm.getPassword()));
 
         User user = new User();
@@ -37,55 +29,35 @@ public class UserService {
 
         userRepository.save(user);
 
-        return ResponseEntity.created(uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri()).body(new UserDto(user));
+        return user;
     }
 
-    public ResponseEntity<?> listAll(Pageable pageable) {
-        Page<User> users = userRepository.findAll(pageable);
-        return UserDto.convertManyToDto(users);
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
-    public ResponseEntity<?> getById(Long id) {
-        Optional<User> optional = userRepository.findById(id);
-
-        if (optional.isPresent())
-            return ResponseEntity.status(HttpStatus.OK).body(new UserDto(optional.get()));
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found.");
+    public Page<User> listAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
-    public ResponseEntity<?> updateById(LoginForm loginForm, Long id) {
-        Optional<User> optional = userRepository.findById(id);
-
-        if (optional.isPresent()) {
-            Optional<User> userByEmail = userRepository.findByEmail(loginForm.getEmail());
-
-            if (userByEmail.isPresent())
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exist.");
-
-            return updateFields(loginForm,optional.get());
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found!");
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
     }
 
-    private ResponseEntity<?> updateFields(LoginForm loginForm, User user) {
+    public User update(LoginForm loginForm, Long id) {
+        return updateFields(loginForm,userRepository.getById(id));
+    }
+
+    private User updateFields(LoginForm loginForm, User user) {
         loginForm.setPassword(new BCryptPasswordEncoder().encode(loginForm.getPassword()));
         BeanUtils.copyProperties(loginForm,user);
 
         userRepository.save(user);
 
-        return ResponseEntity.status(HttpStatus.OK).body(new UserDto(user));
+        return user;
     }
 
-    public ResponseEntity<?> deleteById(Long id) {
-        Optional<User> optional = userRepository.findById(id);
-
-        if (optional.isPresent()) {
-            userRepository.deleteById(id);
-            return ResponseEntity.status(HttpStatus.OK).body("User deleted.");
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User ID not found.");
+    public void delete(User user) {
+        userRepository.delete(user);
     }
 }
